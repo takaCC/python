2ab63eab4a46951b8f45655aa08c528025da8674
takaCC <takaCC@users.noreply.github.com>
2016-02-05 12:31:53 +0900

    Update exportAnimLayer.py

--- exportAnimLayer.py
+++ exportAnimLayer.py
@@ -40,7 +40,7 @@ def exportAnimLayer(path,exportLayer):
     
     cmds.file(path,force=1,exportSelected=1,channels=0,constructionHistory=0,expressions=0,constraints=0,type="mayaBinary")
 
-#指定したアニメーションレイヤーを書き出す関数(全レイヤー).pathは書き出すファイル名のフルパスを記入する
+#指定したアニメーションレイヤーを書き出す関数(全レイヤー)。pathは書き出すファイル名のフルパスを記入する
 def exportAnimLayerALL(path):
     baseLayerName = cmds.animLayer(q=1,root=1)
     exportLayers = []
@@ -53,15 +53,20 @@ def exportAnimLayerALL(path):
     if len(exportLayers):
         layerCurves = []
         layerBlendNodes = []
+        exportCheck = 1
         for work in exportLayers:
             cmds.select(cl=1)
             for Alayer in exportLayers:
                 cmds.animLayer(Alayer,e=1,writeBlendnodeDestinations=1)
-                for node in cmds.animLayer(Alayer,q=1,animCurves=1):
-                	layerCurves.append(node)
-                for node in cmds.animLayer(Alayer,q=1,blendNodes=1):
-                	layerBlendNodes.append(node)
-        cmds.select(exportLayers,add=1,noExpand=1)
-        cmds.select(layerBlendNodes,layerCurves,add=1)
-        cmds.file(path,force=1,exportSelected=1,channels=0,constructionHistory=0,expressions=0,constraints=0,type="mayaBinary")
-        cmds.select(cl=1)
+                #アニメーションレイヤーにアニムカーブやblendNodesが含まれている場合は配列に代入
+                if cmds.animLayer(Alayer,q=1,animCurves=1) != None and cmds.animLayer(Alayer,q=1,blendNodes=1) != None:
+                    for node in cmds.animLayer(Alayer,q=1,animCurves=1):
+                        layerCurves.append(node)
+                    for node in cmds.animLayer(Alayer,q=1,blendNodes=1):
+                        layerBlendNodes.append(node)
+        #アニメーションカーブ等が無い場合、書き出し処理を行わない
+        if len(layerCurves) > 0  and len(layerBlendNodes) > 0:
+            cmds.select(exportLayers,add=1,noExpand=1)
+            cmds.select(layerBlendNodes,layerCurves,add=1)
+            cmds.file(path,force=1,exportSelected=1,channels=0,constructionHistory=0,expressions=0,constraints=0,type="mayaBinary")
+            cmds.select(cl=1)
